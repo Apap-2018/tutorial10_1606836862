b5e9088d63e5ce6558fdff9510c81c3363d94bc9
kemalaAndiyani <kemala.a@gmail.com>
2018-12-05 19:21:33 +0700

    tutorial 10 + latihan 1&2

---
+++ src/screens/DaftarStaffFarmasi.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { DaftarStaffRow } from '../components/DaftarStaffRow';
+import { Loading } from '../components/Loading';
+import { TableContainer } from '../containers/TableContainer';
+import {Appointment} from '../utils/Appointment';
+
+export class DaftarStaffFarmasi extends React.Component {
+	
+	constructor(props) {
+		super(props)
+		this.state = {
+			loading: true,
+			listStaff: []
+		}
+		Appointment.getAllStaffFarmasi().then(response => {
+			this.setState({
+				loading: false,
+				listStaff: response.result
+			})
+		})
+	}
+
+	render() {
+        if (this.state.loading) {
+            return (
+                <Loading msg="Fetching Data..."/>
+            )
+        } else {
+            return (
+                <TableContainer title="Daftar Staff Farmasi" header={['Nama Staff', 'Jenis Staff']}>
+                    <DaftarStaffRow listStaff={this.state.listStaff}/>
+                </TableContainer>
+            )
+        }
+	}
+}
